4d02ffb9a934e4b83780c6b1117e55e1aac7d5e5
nicktalati <nicktalati@gmail.com>
2024-02-10 12:20:51 -0500

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Dave was here.
+          Dave was here, and he is a great guy.
         </p>
         <a
           className="App-link"
